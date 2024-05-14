2472dc39025e8b5e3f72ecfb46e3e59b78b35b7e
sypham <sy130691@gmail.com>
2024-05-14 15:35:30 +0700

    update

---
+++ gkm.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// TODO: Verify if we're getting loaded from multiples location and prevent creating new child processes?
+
+var EventEmitter2 = require('eventemitter2').EventEmitter2;
+var path = require('path');
+var spawn = require('child_process').spawn;
+
+var vnz = {
+	gkmPathDefault: path.join(__dirname, 'lib/gkm.jar'),
+	gkmPath: '',
+	start() {
+		var events = new EventEmitter2({wildcard: true});
+		var gkm = spawn('java', ['-jar', this.gkmPath ? this.gkmPath : this.gkmPathDefault]);
+		gkm.stdout.on('data', function(data) {
+			data = data.toString().split(/\r\n|\r|\n/).filter(function(item) { return item; });
+			for (var i in data) {
+				var parts = data[i].split(':');
+				events.emit(parts[0], parts.slice(1));
+			}
+		});
+		return events
+	}
+}
+
+
+module.exports = vnz;
